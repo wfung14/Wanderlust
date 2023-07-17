99945b91e4fdbd26fc0ceb552c2f23c1d6c220b6
Wilson Fung <wilson_fung1996@yahoo.com>
2023-07-16 21:32:40 -0400

    update intinerary model

--- server/models/intinerary.js
+++ server/models/intinerary.js
@@ -1,14 +1,20 @@
-const { Schema } = require('mongoose')
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
-const itinerarySchema = new Schema ({
-  activity: {
-    type: String,
-    required: true
+const itinerarySchema = new Schema(
+  {
+    activity: {
+      type: String,
+      required: true
+    },
+    date: {
+      type: Date,
+      required: true
+    }
   },
-  date: {
-    type: Date,
-    required: true
+  {
+    timestamps: true,
   }
-})
+)
 
-module.exports = itinerarySchema
+module.exports = mongoose.model('Itinerary', itinerarySchema)
