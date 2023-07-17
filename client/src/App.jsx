22069721b57aa721f135d2003d1716994ac44adb
Wilson Fung <wilson_fung1996@yahoo.com>
2023-07-17 10:01:50 -0400

    multiple fixes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
+import { useState } from 'react'
 import './App.css'
 
 const App = () => {
 
+
   return (
-    <div className='App'>
+    <main className='App'>
       test
-    </div>
+    </main>
   )
 }
 
