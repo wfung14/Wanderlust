db83e41d7adf8d178c574ef5921061a66e1c6ffd
Wilson Fung <wilson_fung1996@yahoo.com>
2023-07-16 22:40:03 -0400

    update middlewar/index.js

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -1,15 +1,58 @@
-const mongoose = require('mongoose')
-require('dotenv').config()
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+require("dotenv").config()
 
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log('Successfully connected to MongoDB . . .')
-  })
-  .catch((e) => {
-    console.error('Connection error', e.message)
-  })
+const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS)
+const APP_SECRET = process.env.APP_SECRET
 
-const db = mongoose.connection
+const hashPassword = async (password) => {
+  let hashedPassword = await bcrypt.hash(password, SALT_ROUNDS)
+  return hashedPassword
+}
 
-module.exports = db
+const comparePassword = async (storedPassword, password) => {
+  let passwordMatch = await bcryot.compare(password, storedPassword)
+  return passwordMatch
+}
+
+const stripToken = (req, res, next) => {
+  try {
+    const token = req.headers["authrorization"].split("")[1]
+    if (token) {
+      res.locals.token = token
+      return next()
+    }
+    res.status(401).send({ status: "Error", msg: "Unauthorized" })
+  } catch (error) {
+    console.log(error)
+    res.status(401).send({ status: "Error", msg: "Strip Token Error" })
+  }
+}
+
+const verifyToken = (req, res, next) => {
+  const { token } = res.locals
+  try {
+    let payload = jwt.verify(token, APP_SECRET)
+    if (payload) {
+      res.locals.payload = payload
+      return next()
+    }
+    res.status(401).send({ status: "Error", msg: "Unauthorized" })
+  } catch (error) {
+    console.log(error)
+    res.status(401).send({ status: "Error", msg: "Verify Token Error!" })
+  }
+}
+
+const createToken = (payload) => {
+  let token = jwt.sign(payload, APP_SECRET)
+  return token
+}
+
+module.exports = {
+  hashPassword,
+  comparePassword,
+  stripToken,
+  verifyToken,
+  createToken,
+}
