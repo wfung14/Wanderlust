bbc4ebefe00e9a983fe6e6b5b02598646a648316
Wilson Fung <wilson_fung1996@yahoo.com>
2023-07-16 21:24:18 -0400

    update note model

--- server/models/note.js
+++ server/models/note.js
@@ -1,9 +1,13 @@
-const { Schema } = require('mongoose')
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
 const noteSchema = new Schema ({
   content: {
     type: String
+  },
+  {
+    timestamps: true
   }
 })
 
-module.exports = noteSchema
+module.exports = mongoose.model('Note', noteSchema);
