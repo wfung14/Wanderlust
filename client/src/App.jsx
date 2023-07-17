e0f49f22394c87fcd669b1f6491d17c08915972b
Wilson Fung <wilson_fung1996@yahoo.com>
2023-07-17 13:20:31 -0400

    finish react auth possibly

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,30 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import Register from './pages/Register'
+import SignIn from './pages/SignIn'
+import { CheckSession } from './services/Auth'
 
 import './App.css'
 
 const App = () => {
 
+  const [user, setUser] = useState(null)
+
+  const handleLogOut = () => {
+    setUser(null)
+    localStorage.clear()
+  }
+
+  const checkToken = async () => {
+    const user = await CheckSession()
+    setUser(user)
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      checkToken()
+    }
+  }, [])
 
   return (
     <main className="App">
