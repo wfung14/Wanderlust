50e647334a40ce34eabb896e721fc18ec23b98cc
Wilson Fung <wilson_fung1996@yahoo.com>
2023-07-17 02:03:43 -0400

    add intinerary controller

---
+++ controllers/itinerary.js
@@ -0,0 +1,28 @@
+const Location = require('../models/location');
+
+const create = async (req, res) => {
+  const location = await Location.findById(req.params.id);
+  location.itinerary.push(req.body);
+  try{
+    await location.save();
+  } catch (err) {
+    res.redirect('/locations/${location._id}')
+  }
+}
+
+const deleteItinerary = (req, res, next) => {
+  Location.findOne({'itinerary._id': req.params.id, 'itinerary.user': req.user._id}).then(function(location) {
+    if (!location) return res.redirect('/locations');
+    location.itinerary.remove(req.params.id);
+    location.save().then(function() {
+      res.redirect(`/locations/${location._id}`);
+    }).catch(function(err) {
+      return next(err);
+    });
+  });
+}
+
+module.exports = {
+  create,
+  delete: deleteItinerary
+}
